8f3ad495b92e417cc3e54892ea8a33cacb66a7a7
cesar018 <cesitarvargas007@gmail.com>
2020-10-22 22:29:48 -0500

    commit angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioService } from './Services/usuario.service';
+import { FormsModule } from '@angular/forms';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { ListadoUsuariosComponent } from './listado-usuarios/listado-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,UsuarioComponent, ListadoUsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
